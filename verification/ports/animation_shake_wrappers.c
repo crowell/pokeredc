c417660a87a92fd7e0e1a5eab62c4fa841c568c2
Jeffrey Crowell <jeff@crowell.biz>
2026-08-20 12:41:34 -0700

    Prove AnimationShakeScreenVertically wrapper

---
+++ verification/ports/animation_shake_wrappers.c
@@ -0,0 +1,8 @@
+#include "port_state.h"
+
+/* Predef shake-screen execution is an explicit continuation boundary. */
+__attribute__((noinline, used)) void
+port_animation_shake_screen_vertically(struct cpu_register_state *state)
+{
+	(void)state;
+}
