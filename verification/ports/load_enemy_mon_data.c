8a7bec050484a8cffe5c0a08c593c7c8ace0bc9c
Jeffrey Crowell <jeff@crowell.biz>
2026-08-21 01:01:41 -0700

    Port LoadEnemyMonData link branch

---
+++ verification/ports/load_enemy_mon_data.c
@@ -0,0 +1,18 @@
+#include "port_state.h"
+
+struct load_enemy_mon_data_state {
+	struct cpu_register_state registers;
+	port_u8 link_state;
+};
+
+/* Port of LoadEnemyMonData through the link-battle branch. */
+__attribute__((noinline, used)) void
+port_load_enemy_mon_data(struct load_enemy_mon_data_state *state)
+{
+	port_u8 value = state->link_state;
+	state->registers.a = value;
+	state->registers.f = (port_u8)(PORT_FLAG_N |
+		((port_u8)((value & 0x0f) < 4) * PORT_FLAG_H) |
+		((port_u8)(value < 4) * PORT_FLAG_C) |
+		((port_u8)(value == 4) * PORT_FLAG_Z));
+}
