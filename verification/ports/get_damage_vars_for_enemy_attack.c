59f1317ed6e4a6075eefaab5270c88a32f33846e
Jeffrey Crowell <jeff@crowell.biz>
2026-08-20 23:21:06 -0700

    Port enemy damage variable setup

---
+++ verification/ports/get_damage_vars_for_enemy_attack.c
@@ -0,0 +1,18 @@
+#include "port_state.h"
+
+struct get_damage_vars_enemy_state {
+	struct cpu_register_state registers;
+	port_u8 move_power;
+};
+
+/* Port of GetDamageVarsForEnemyAttack through the zero-power return. */
+__attribute__((noinline, used)) void
+port_get_damage_vars_for_enemy_attack(struct get_damage_vars_enemy_state *state)
+{
+	state->registers.h = 0xcf;
+	state->registers.l = 0xcf;
+	state->registers.a = state->move_power;
+	state->registers.d = state->move_power;
+	state->registers.f = (port_u8)(PORT_FLAG_H |
+		((port_u8)(state->move_power == 0) * PORT_FLAG_Z));
+}
