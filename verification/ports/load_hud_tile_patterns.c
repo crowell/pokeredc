a51fce7dab2e6cd47824fdf2d35617513cc28f18
Jeffrey Crowell <jeff@crowell.biz>
2026-08-21 01:29:32 -0700

    Port LoadHudTilePatterns LCD branch

---
+++ verification/ports/load_hud_tile_patterns.c
@@ -0,0 +1,22 @@
+#include "port_state.h"
+
+struct load_hud_tile_patterns_state {
+	struct cpu_register_state registers;
+	port_u8 lcd_control;
+};
+
+/* Port of LoadHudTilePatterns through the LCD branch. */
+__attribute__((noinline, used)) void
+port_load_hud_tile_patterns(struct load_hud_tile_patterns_state *state)
+{
+	port_u8 old = state->lcd_control;
+	port_u16 wide = (port_u16)old + old;
+	state->registers.a = (port_u8)wide;
+	state->registers.f = 0;
+	if (state->registers.a == 0)
+		state->registers.f |= PORT_FLAG_Z;
+	if ((old & 0x0f) + (old & 0x0f) > 0x0f)
+		state->registers.f |= PORT_FLAG_H;
+	if (wide > 0xff)
+		state->registers.f |= PORT_FLAG_C;
+}
