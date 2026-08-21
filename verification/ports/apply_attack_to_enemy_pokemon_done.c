9b6d2c74a1fdaf0f862f6afe6a60a7039765572a
Jeffrey Crowell <jeff@crowell.biz>
2026-08-20 23:39:04 -0700

    Port enemy attack completion wrapper

---
+++ verification/ports/apply_attack_to_enemy_pokemon_done.c
@@ -0,0 +1,8 @@
+#include "port_state.h"
+
+/* Port of ApplyAttackToEnemyPokemonDone through DrawHUDsAndHPBars. */
+__attribute__((noinline, used)) void
+port_apply_attack_to_enemy_pokemon_done(struct cpu_register_state *registers)
+{
+	(void)registers;
+}
