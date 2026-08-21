862a1b52fc937c925900bc36e6b4e6b1feddbcc4
Jeffrey Crowell <jeff@crowell.biz>
2026-08-21 05:12:59 -0700

    Port PrintSafariZoneBattleText branches

---
+++ verification/ports/print_safari_zone_battle_text_private.c
@@ -0,0 +1,40 @@
+#include "port_state.h"
+
+struct print_safari_zone_battle_text_private_state {
+	struct cpu_register_state registers;
+	port_u8 bait_factor;
+	port_u8 rock_factor;
+};
+
+static port_u8
+	safari_dec_flags(port_u8 old_value)
+{
+	port_u8 result = (port_u8)(old_value - 1);
+	return (port_u8)(((port_u8)(result == 0) * 0x80) |
+		((port_u8)((old_value & 0x0f) == 0) * 0x20));
+}
+
+/* Port of PrintSafariZoneBattleText through its bait/rock text selection. */
+__attribute__((noinline, used)) void
+port_print_safari_zone_battle_text_private(
+	struct print_safari_zone_battle_text_private_state *state)
+{
+	port_u8 old_value;
+	if (state->bait_factor != 0) {
+		old_value = state->bait_factor;
+		state->bait_factor = (port_u8)(old_value - 1);
+		state->registers.a = old_value;
+		state->registers.h = 0x42;
+		state->registers.l = 0xa7;
+		state->registers.f = safari_dec_flags(old_value);
+		return;
+	}
+	old_value = state->rock_factor;
+	state->rock_factor = (port_u8)(old_value - 1);
+	state->registers.a = old_value;
+	state->registers.f = safari_dec_flags(old_value);
+	if (state->rock_factor != 0) {
+		state->registers.h = 0x42;
+		state->registers.l = 0xac;
+	}
+}
