ea68c62d160a9d8f8ca9a3b6f09a22aa17d411de
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 22:51:51 -0700

    Route AnimationResetScreenPalette through shared helper

--- verification/ports/animation_reset_screen_palette.c
+++ verification/ports/animation_reset_screen_palette.c
@@ -1,7 +1,7 @@
 #include "port_state.h"
 
-#define W_ON_SGB 0xcf1b
-#define R_BGP 0xff47
+void port_set_animation_bg_palette(
+	struct cpu_register_state *, port_u8 *);
 
 /* Port of AnimationResetScreenPalette in engine/battle/animations.asm. */
 __attribute__((noinline, used)) void
@@ -10,10 +10,5 @@ port_animation_reset_screen_palette(
 {
 	state->b = 0xe4;
 	state->c = 0xe4;
-	state->a = memory[W_ON_SGB];
-	state->f = PORT_FLAG_H;
-	if (state->a == 0)
-		state->f |= PORT_FLAG_Z;
-	state->a = state->a == 0 ? state->b : state->c;
-	memory[R_BGP] = state->a;
+	port_set_animation_bg_palette(state, memory);
 }
