28f71c7373ec464772b1719f67086a8fd3af5dad
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:25:19 -0700

    Complete PrintDoesntAffectText port

--- verification/ports/print_doesnt_affect_text.c
+++ verification/ports/print_doesnt_affect_text.c
@@ -1,9 +1,13 @@
 #include "port_state.h"
 
-/* Port of PrintDoesntAffectText through PrintText. */
+void port_print_text(struct cpu_register_state *, port_u8 *);
+
+/* Port of PrintDoesntAffectText in engine/battle/core.asm. */
 __attribute__((noinline, used)) void
-port_print_doesnt_affect_text(struct cpu_register_state *registers)
+port_print_doesnt_affect_text(
+	struct cpu_register_state *registers, port_u8 *memory)
 {
 	registers->h = 0x5c;
 	registers->l = 0x57;
+	port_print_text(registers, memory);
 }
