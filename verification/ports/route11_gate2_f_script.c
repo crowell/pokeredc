11ddd025d4a2c66f41b59ae56f0be7a61a5b34e8
Jeffrey Crowell <jeff@crowell.biz>
2026-08-23 04:52:12 -0700

    Complete Route11Gate2F_Script port
    Three-byte script tail (jp DisableAutoTextBoxDrawing) composed onto the proven
    port_disable_auto_text_box_drawing. The angr test proves all registers, flags, and the hand-off
    snapshot over symbolic inputs.

---
+++ verification/ports/route11_gate2_f_script.c
@@ -0,0 +1,31 @@
+#include "port_state.h"
+
+/* Port of Route11Gate2F_Script in scripts/Route11Gate2F.asm:
+ *
+ *   jp DisableAutoTextBoxDrawing
+ */
+
+void port_disable_auto_text_box_drawing(struct auto_text_box_state *);
+
+#define W_AUTO_TEXT_BOX_DRAWING_CONTROL 0xcf0cu
+#define W_DO_NOT_WAIT_FOR_BUTTON_PRESS  0xcc3cu
+
+__attribute__((noinline, used)) void
+port_route11_gate2_f_script(struct cpu_register_state *state, port_u8 *memory)
+{
+	struct auto_text_box_state text_box;
+
+	text_box.registers = *state;
+	text_box.auto_text_box_drawing_control =
+	    memory[W_AUTO_TEXT_BOX_DRAWING_CONTROL];
+	text_box.do_not_wait_for_button_press =
+	    memory[W_DO_NOT_WAIT_FOR_BUTTON_PRESS];
+
+	port_disable_auto_text_box_drawing(&text_box);
+
+	*state = text_box.registers;
+	memory[W_AUTO_TEXT_BOX_DRAWING_CONTROL] =
+	    text_box.auto_text_box_drawing_control;
+	memory[W_DO_NOT_WAIT_FOR_BUTTON_PRESS] =
+	    text_box.do_not_wait_for_button_press;
+}
