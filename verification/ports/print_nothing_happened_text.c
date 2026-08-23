fa182edd3e8c9c05535067c79c933149d7162d42
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:26:42 -0700

    Complete PrintNothingHappenedText port

--- verification/ports/print_nothing_happened_text.c
+++ verification/ports/print_nothing_happened_text.c
@@ -1,13 +1,13 @@
 #include "port_state.h"
 
 extern void port_print_text(struct cpu_register_state *state, port_u8 *memory);
-static port_u8 nothing_text_memory[0x10000];
 
-/* Port of PrintNothingHappenedText through PrintText. */
+/* Port of PrintNothingHappenedText in engine/battle/effects.asm. */
 __attribute__((noinline, used)) void
-port_print_nothing_happened_text(struct cpu_register_state *registers)
+port_print_nothing_happened_text(
+	struct cpu_register_state *registers, port_u8 *memory)
 {
 	registers->h = 0x7b;
 	registers->l = 0x3e;
-	port_print_text(registers, nothing_text_memory);
+	port_print_text(registers, memory);
 }
