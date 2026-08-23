6ca7a943149246c8c4c256b51159d67e81886a2c
Jeffrey Crowell <jeff@crowell.biz>
2026-08-23 11:10:27 -0700

    Complete UndergroundPathRoute8_Script port
    Underground-path script stub: sets wLastMap to the route
    constant and tails into the proven EnableAutoTextBoxDrawing.
    The angr test proves registers, flags, wLastMap, control RAM,
    and the tail-call input snapshot over symbolic inputs.

---
+++ verification/ports/underground_path_route8_script.c
@@ -0,0 +1,36 @@
+#include "port_state.h"
+
+/* Port of UndergroundPathRoute8Script in UndergroundPathRoute8Script_script.asm equivalent:
+ *   ld a, 0x13
+ *   ld [wLastMap], a
+ *   jp EnableAutoTextBoxDrawing
+ */
+
+void port_enable_auto_text_box_drawing(struct auto_text_box_state *);
+
+#define W_LAST_MAP               0xd365u
+#define W_AUTO_TEXT_BOX_DRAWING_CONTROL 0xcf0cu
+#define W_DO_NOT_WAIT_FOR_BUTTON_PRESS  0xcc3cu
+
+__attribute__((noinline, used)) void
+port_underground_path_route8_script(struct cpu_register_state *state, port_u8 *memory)
+{
+	struct auto_text_box_state text_box;
+
+	state->a = 0x13;
+	memory[W_LAST_MAP] = state->a;
+
+	text_box.registers = *state;
+	text_box.auto_text_box_drawing_control =
+	    memory[W_AUTO_TEXT_BOX_DRAWING_CONTROL];
+	text_box.do_not_wait_for_button_press =
+	    memory[W_DO_NOT_WAIT_FOR_BUTTON_PRESS];
+
+	port_enable_auto_text_box_drawing(&text_box);
+
+	*state = text_box.registers;
+	memory[W_AUTO_TEXT_BOX_DRAWING_CONTROL] =
+	    text_box.auto_text_box_drawing_control;
+	memory[W_DO_NOT_WAIT_FOR_BUTTON_PRESS] =
+	    text_box.do_not_wait_for_button_press;
+}
