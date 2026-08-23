2a479e3abf06f3b08f7980c999768f9e9f686acd
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 22:50:28 -0700

    Route AnimationDarkScreenPalette through shared helper

--- verification/ports/animation_dark_screen_palette.c
+++ verification/ports/animation_dark_screen_palette.c
@@ -1,7 +1,7 @@
 #include "port_state.h"
 
-#define W_ON_SGB 0xcf1b
-#define R_BGP 0xff47
+void port_set_animation_bg_palette(
+	struct cpu_register_state *, port_u8 *);
 
 /* Port of AnimationDarkScreenPalette in engine/battle/animations.asm. */
 __attribute__((noinline, used)) void
@@ -9,10 +9,5 @@ port_animation_dark_screen_palette_player(struct cpu_register_state *state, port
 {
 	state->b = 0x6f;
 	state->c = 0x6f;
-	state->a = memory[W_ON_SGB];
-	state->f = PORT_FLAG_H;
-	if (state->a == 0)
-		state->f |= PORT_FLAG_Z;
-	state->a = state->a == 0 ? state->b : state->c;
-	memory[R_BGP] = state->a;
+	port_set_animation_bg_palette(state, memory);
 }
