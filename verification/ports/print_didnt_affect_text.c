bc92f8e3f7c0e9ef4f9d1ef67930f3ee7496b909
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:28:34 -0700

    Complete PrintDidntAffectText port

--- verification/ports/print_didnt_affect_text.c
+++ verification/ports/print_didnt_affect_text.c
@@ -1,12 +1,13 @@
 #include "port_state.h"
 
 extern void port_print_text(struct cpu_register_state *state, port_u8 *memory);
-static port_u8 didnt_affect_memory[0x10000];
 
+/* Port of PrintDidntAffectText in engine/battle/effects.asm. */
 __attribute__((noinline, used)) void
-port_print_didnt_affect_text(struct cpu_register_state *registers)
+port_print_didnt_affect_text(
+	struct cpu_register_state *registers, port_u8 *memory)
 {
 	registers->h = 0x7b;
 	registers->l = 0x64;
-	port_print_text(registers, didnt_affect_memory);
+	port_print_text(registers, memory);
 }
