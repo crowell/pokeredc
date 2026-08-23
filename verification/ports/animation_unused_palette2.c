a951bbbf5c018a506134c36acb5b864ea199738d
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 22:54:22 -0700

    Route AnimationUnusedPalette2 through shared helper

--- verification/ports/animation_unused_palette2.c
+++ verification/ports/animation_unused_palette2.c
@@ -1,7 +1,7 @@
 #include "port_state.h"
 
-#define R_BGP 0xff47
-#define W_ON_SGB 0xcf1b
+void port_set_animation_bg_palette(
+	struct cpu_register_state *, port_u8 *);
 
 /* Port of AnimationUnusedPalette2 in engine/battle/animations.asm. */
 __attribute__((noinline, used)) void
@@ -9,10 +9,5 @@ port_animation_unused_palette2_player(struct cpu_register_state *state, port_u8 
 {
 	state->b = 0xff;
 	state->c = 0xff;
-	state->a = memory[W_ON_SGB];
-	state->f = PORT_FLAG_H;
-	if (state->a == 0)
-		state->f |= PORT_FLAG_Z;
-	state->a = state->a == 0 ? state->b : state->c;
-	memory[R_BGP] = state->a;
+	port_set_animation_bg_palette(state, memory);
 }
