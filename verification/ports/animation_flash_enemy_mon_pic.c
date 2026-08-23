d92d07c310ef5bae9c0f243564912ce36733ad52
Jeffrey Crowell <jeff@crowell.biz>
2026-08-23 10:46:40 -0700

    Complete AnimationFlashEnemyMonPic and AnimationShowEnemyMonPic ports
    Two animation thunks (ld hl,<pic label>; jp CallWithTurnFlipped) composed
    onto the proven port_call_with_turn_flipped. The angr tests prove
    registers, flags, and the tail-call hand-off snapshot over all symbolic
    inputs.

---
+++ verification/ports/animation_flash_enemy_mon_pic.c
@@ -0,0 +1,17 @@
+#include "port_state.h"
+
+/* Port of AnimationFlashEnemyMonPic in engine/battle/animations.asm:
+ *
+ *   ld hl, 0x5389
+ *   jp CallWithTurnFlipped
+ */
+
+void port_call_with_turn_flipped(struct cpu_register_state *, port_u8 *);
+
+__attribute__((noinline, used)) void
+port_animation_flash_enemy_mon_pic(struct cpu_register_state *state, port_u8 *memory)
+{
+	state->h = 0x53u;
+	state->l = 0x89u;
+	port_call_with_turn_flipped(state, memory);
+}
---
+++ verification/ports/animation_show_enemy_mon_pic.c
@@ -0,0 +1,17 @@
+#include "port_state.h"
+
+/* Port of AnimationShowEnemyMonPic in engine/battle/animations.asm:
+ *
+ *   ld hl, 0x539e
+ *   jp CallWithTurnFlipped
+ */
+
+void port_call_with_turn_flipped(struct cpu_register_state *, port_u8 *);
+
+__attribute__((noinline, used)) void
+port_animation_show_enemy_mon_pic(struct cpu_register_state *state, port_u8 *memory)
+{
+	state->h = 0x53u;
+	state->l = 0x9eu;
+	port_call_with_turn_flipped(state, memory);
+}
