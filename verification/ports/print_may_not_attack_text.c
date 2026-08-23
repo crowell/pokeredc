4f0013b9aeef187896ae8efb32e56eb8edc4226c
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:29:23 -0700

    Complete PrintMayNotAttackText port

--- verification/ports/print_may_not_attack_text.c
+++ verification/ports/print_may_not_attack_text.c
@@ -1,12 +1,13 @@
 #include "port_state.h"
 
 extern void port_print_text(struct cpu_register_state *state, port_u8 *memory);
-static port_u8 may_not_attack_memory[0x10000];
 
+/* Port of PrintMayNotAttackText in engine/battle/effects.asm. */
 __attribute__((noinline, used)) void
-port_print_may_not_attack_text(struct cpu_register_state *registers)
+port_print_may_not_attack_text(
+	struct cpu_register_state *registers, port_u8 *memory)
 {
 	registers->h = 0x7b;
 	registers->l = 0x74;
-	port_print_text(registers, may_not_attack_memory);
+	port_print_text(registers, memory);
 }
