d128c33124562210466c6df71ff3c4ad3065644d
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:35:58 -0700

    Complete DisplayUsedMoveText port

--- verification/ports/display_used_move_text_private.c
+++ verification/ports/display_used_move_text_private.c
@@ -1,14 +1,13 @@
 #include "port_state.h"
 
-struct display_used_move_text_private_state {
-	struct cpu_register_state registers;
-};
+void port_print_text(struct cpu_register_state *, port_u8 *);
 
-/* Port of DisplayUsedMoveText through UsedMoveText pointer setup. */
+/* Port of DisplayUsedMoveText in engine/battle/used_move_text.asm. */
 __attribute__((noinline, used)) void
 port_display_used_move_text_private(
-	struct display_used_move_text_private_state *state)
+	struct cpu_register_state *state, port_u8 *memory)
 {
-	state->registers.h = 0x5a;
-	state->registers.l = 0xfb;
+	state->h = 0x5a;
+	state->l = 0xfb;
+	port_print_text(state, memory);
 }
