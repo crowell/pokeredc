1bbff1e4d7c1e45fa86b23dec581224412728915
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:40:13 -0700

    Complete UsedCut.nothingToCut port

--- verification/ports/used_cut_nothing_to_cut.c
+++ verification/ports/used_cut_nothing_to_cut.c
@@ -1,15 +1,15 @@
 #include "port_state.h"
 
-/* Port of UsedCut.nothingToCut in engine/overworld/cut.asm.
- *
- * ld hl, $6f7d; jp $3c49. LD HL and JP preserve F; the local PrintText JP is the boundary. */
+/* Port of UsedCut.nothingToCut in engine/overworld/cut.asm. */
 
 #define USED_CUT_NOTHING_TO_CUT_HL 0x6f7du
+
+void port_print_text(struct cpu_register_state *, port_u8 *);
 
 __attribute__((noinline, used)) void
 port_used_cut_nothing_to_cut(struct cpu_register_state *state, port_u8 *memory)
 {
-    (void)memory;
-    state->h = (port_u8)(USED_CUT_NOTHING_TO_CUT_HL >> 8);
-    state->l = (port_u8)(USED_CUT_NOTHING_TO_CUT_HL & 0xff);
+	state->h = (port_u8)(USED_CUT_NOTHING_TO_CUT_HL >> 8);
+	state->l = (port_u8)(USED_CUT_NOTHING_TO_CUT_HL & 0xff);
+	port_print_text(state, memory);
 }
