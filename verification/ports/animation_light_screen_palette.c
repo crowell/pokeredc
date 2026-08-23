1409405fa1f9741797a8d64287ba470c0512696d
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 22:48:22 -0700

    Port AnimationLightScreenPalette

---
+++ verification/ports/animation_light_screen_palette.c
@@ -0,0 +1,19 @@
+#include "port_state.h"
+
+#define W_ON_SGB 0xcf1b
+#define R_BGP 0xff47
+
+/* Port of AnimationLightScreenPalette in engine/battle/animations.asm. */
+__attribute__((noinline, used)) void
+port_animation_light_screen_palette(
+	struct cpu_register_state *state, port_u8 *memory)
+{
+	state->b = 0x90;
+	state->c = 0x90;
+	state->a = memory[W_ON_SGB];
+	state->f = PORT_FLAG_H;
+	if (state->a == 0)
+		state->f |= PORT_FLAG_Z;
+	state->a = state->a == 0 ? state->b : state->c;
+	memory[R_BGP] = state->a;
+}
