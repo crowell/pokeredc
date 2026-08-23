b4590596e68226730c06f32a485651e97a19611c
Jeffrey Crowell <jeff@crowell.biz>
2026-08-23 10:52:11 -0700

    Complete FuchsiaMeetingRoom_Script port
    Map-script stub: calls the proven EnableAutoTextBoxDrawing
    bridge and returns. The angr test proves registers, flags, control RAM,
    and the tail-call input snapshot over all symbolic inputs.

---
+++ verification/ports/fuchsia_meeting_room_script.c
@@ -0,0 +1,27 @@
+#include "port_state.h"
+
+/* Port of FuchsiaMeetingRoom_Script in scripts/FuchsiaMeetingRoom.asm:
+ *
+ *   call EnableAutoTextBoxDrawing
+ *   ret
+ */
+
+void port_enable_auto_text_box_drawing(struct auto_text_box_state *);
+
+__attribute__((noinline, used)) void
+port_fuchsia_meeting_room_script(struct cpu_register_state *state, port_u8 *memory)
+{
+	struct auto_text_box_state text_box;
+
+	text_box.registers = *state;
+	text_box.auto_text_box_drawing_control =
+	    memory[0xcf0cu];
+	text_box.do_not_wait_for_button_press =
+	    memory[0xcc3cu];
+
+	port_enable_auto_text_box_drawing(&text_box);
+
+	*state = text_box.registers;
+	memory[0xcf0cu] = text_box.auto_text_box_drawing_control;
+	memory[0xcc3cu] = text_box.do_not_wait_for_button_press;
+}
