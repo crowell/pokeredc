7b0cd1cea0df54eb152dfab02604ef7e4732e4e8
Jeffrey Crowell <jeff@crowell.biz>
2026-08-22 23:27:35 -0700

    Complete PrintNoEffectText port

--- verification/ports/print_no_effect_text.c
+++ verification/ports/print_no_effect_text.c
@@ -1,12 +1,13 @@
 #include "port_state.h"
 
 extern void port_print_text(struct cpu_register_state *state, port_u8 *memory);
-static port_u8 no_effect_memory[0x10000];
 
+/* Port of PrintNoEffectText in engine/battle/effects.asm. */
 __attribute__((noinline, used)) void
-port_print_no_effect_text(struct cpu_register_state *registers)
+port_print_no_effect_text(
+	struct cpu_register_state *registers, port_u8 *memory)
 {
 	registers->h = 0x7b;
 	registers->l = 0x49;
-	port_print_text(registers, no_effect_memory);
+	port_print_text(registers, memory);
 }
