55d831f88bb43e9b9f45c0332269ddf73912cc03
Jeffrey Crowell <jeff@crowell.biz>
2026-08-25 00:39:48 -0400

    Complete LoadHudAndHpBarAndStatusTilePatterns port

--- verification/ports/load_hud_and_hp_bar_and_status_tile_patterns.c
+++ verification/ports/load_hud_and_hp_bar_and_status_tile_patterns.c
@@ -1,22 +1,26 @@
 #include "port_state.h"
 
-struct load_hud_patterns_state {
-	struct cpu_register_state registers;
-	port_u8 lcd_control;
-};
+void port_load_hp_bar_and_status_tile_patterns(
+	struct load_hp_bar_tile_patterns_state *, port_u8 *);
+void port_load_hud_tile_patterns(struct load_hud_tile_patterns_state *,
+	port_u8 *);
 
-/* Port of LoadHudAndHpBarAndStatusTilePatterns through LCD selection. */
+/* Port of LoadHudAndHpBarAndStatusTilePatterns in engine/battle/core.asm. */
 __attribute__((noinline, used)) void
-port_load_hud_and_hp_bar_and_status_tile_patterns(struct load_hud_patterns_state *state)
+port_load_hud_and_hp_bar_and_status_tile_patterns(
+	struct load_hud_tile_patterns_state *state, port_u8 *memory)
 {
-	port_u8 old = state->lcd_control;
-	port_u16 wide = (port_u16)old + old;
-	state->registers.a = (port_u8)wide;
-	state->registers.f = 0;
-	if (state->registers.a == 0)
-		state->registers.f |= PORT_FLAG_Z;
-	if ((old & 0x0f) + (old & 0x0f) > 0x0f)
-		state->registers.f |= PORT_FLAG_H;
-	if (wide > 0xff)
-		state->registers.f |= PORT_FLAG_C;
+	struct load_hp_bar_tile_patterns_state hp;
+
+	hp.transfer.registers = state->transfer.registers;
+	hp.transfer.requested_bank = state->transfer.rom_bank_temp;
+	hp.transfer.loaded_bank = state->transfer.loaded_rom_bank;
+	hp.transfer.rom_bank = state->transfer.mapper_bank;
+	hp.lcd_control = state->lcd_control;
+	port_load_hp_bar_and_status_tile_patterns(&hp, memory);
+	state->transfer.registers = hp.transfer.registers;
+	state->transfer.rom_bank_temp = hp.transfer.requested_bank;
+	state->transfer.loaded_rom_bank = hp.transfer.loaded_bank;
+	state->transfer.mapper_bank = hp.transfer.rom_bank;
+	port_load_hud_tile_patterns(state, memory);
 }
