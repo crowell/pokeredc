a39eed1cead79b1a4b08ab38e21bb9573c0b130b
Jeffrey Crowell <jeff@crowell.biz>
2026-08-25 00:27:15 -0400

    Complete LoadPartyPokeballGfx port

--- verification/ports/load_party_pokeball_gfx.c
+++ verification/ports/load_party_pokeball_gfx.c
@@ -1,13 +1,30 @@
 #include "port_state.h"
 
-/* Port of LoadPartyPokeballGfx through CopyVideoData. */
-__attribute__((noinline, used)) void
-port_load_party_pokeball_gfx(struct cpu_register_state *registers)
+void port_copy_video_data(struct cpu_register_state *, port_u8 *);
+
+static void
+load_party_pokeball_gfx_setup(struct cpu_register_state *registers)
 {
 	registers->d = 0x69;
 	registers->e = 0x7e;
 	registers->h = 0x83;
 	registers->l = 0x10;
-	registers->b = 4;
-	registers->c = 0x0e;
+	registers->b = 0x0e;
+	registers->c = 4;
+}
+
+/* Compatibility setup used by older partial callers. */
+__attribute__((noinline, used)) void
+port_load_party_pokeball_gfx(struct cpu_register_state *registers)
+{
+	load_party_pokeball_gfx_setup(registers);
+}
+
+/* Port of LoadPartyPokeballGfx in draw_hud_pokeball_gfx.asm. */
+__attribute__((noinline, used)) void
+port_load_party_pokeball_gfx_with_memory(
+	struct cpu_register_state *registers, port_u8 *memory)
+{
+	load_party_pokeball_gfx_setup(registers);
+	port_copy_video_data(registers, memory);
 }
