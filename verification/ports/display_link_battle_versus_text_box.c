946f0b82ab45490d3f0537f90a36323fe5e6bd98
Jeffrey Crowell <jeff@crowell.biz>
2026-08-29 22:55:06 -0400

    Compose link battle textbox border

--- verification/ports/display_link_battle_versus_text_box.c
+++ verification/ports/display_link_battle_versus_text_box.c
@@ -1,11 +1,19 @@
 #include "port_state.h"
 
-/* Port of DisplayLinkBattleVersusTextBox through TextBoxBorder. */
+void port_text_box_border(struct text_box_border_state *, port_u8 *);
+
+/* Port of DisplayLinkBattleVersusTextBox through the complete TextBoxBorder
+ * callee.  LoadTextBoxTilePatterns remains an explicit graphics boundary. */
 __attribute__((noinline, used)) void
-port_display_link_battle_versus_text_box(struct cpu_register_state *registers)
+port_display_link_battle_versus_text_box(struct cpu_register_state *registers,
+	port_u8 *memory)
 {
-	registers->h = 0xc3;
-	registers->l = 0xf3;
-	registers->b = 7;
-	registers->c = 12;
+	struct text_box_border_state border = {0};
+	border.registers = *registers;
+	border.registers.h = 0xc3;
+	border.registers.l = 0xf3;
+	border.registers.b = 7;
+	border.registers.c = 12;
+	port_text_box_border(&border, memory);
+	*registers = border.registers;
 }
